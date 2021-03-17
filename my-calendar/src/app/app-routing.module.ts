504690b29400abec723de8bd15293afcfcb03a2d
Aitor <aitor.tapia92@gmail.com>
2021-03-17 17:22:48 +0100

    Calendario

---
+++ my-calendar/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {CalendarComponent} from "./page/calendar/calendar.component";
+
+const routes: Routes = [
+  { path: 'calendar',   component: CalendarComponent },
+  {
+    path: '**',
+    redirectTo: 'calendar'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
